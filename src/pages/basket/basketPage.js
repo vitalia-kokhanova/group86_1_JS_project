fcdd1f8461a51052883f5d764be556e088505274
Vitalia Kokhanova <vitalia.kokhanova@gmail.com>
2024-09-24 16:55:50 +0300

    Добавила правки по блоку First Screen, улучшила каркас страниц Контакты и Корзина + мелкие правки.

--- src/pages/basket/basketPage.js
+++ src/pages/basket/basketPage.js
@@ -1,6 +1,8 @@
 import "./_basketPage.scss";
+
 import { renderHeader } from "../../common/header";
 import { renderMainContainer } from "../../common/common";
+import { renderFirstScreen } from "../../common/firstScreen";
 import { renderFooter } from "../../common/footer";
 
 export function renderBasket(element) {
@@ -8,7 +10,36 @@ export function renderBasket(element) {
 	renderHeader();
 	renderMainContainer();
 	const mainContainer = document.querySelector(".main");
-	mainContainer.innerHTML = "Корзина";
+	renderFirstScreen(mainContainer);
 
+	const innerWrapper = document.querySelector(".first-screen__inner-wraper");
+	innerWrapper.innerHTML = `
+    <h2 class="secondary-main-title">Корзина</h2>
+    `;
+	innerWrapper.classList.add("basket-page__inner-wrapper_position");
+	renderTotalSumBlock(mainContainer);
+	renderCheckoutForm(mainContainer);
 	renderFooter();
+}
+
+function renderTotalSumBlock(element) {
+	const template = `
+        <section class="totalsum-block">
+			<div class="container totalsum-block__container">
+				<h3> Сумма заказов </h3>
+			</div>
+		</section>
+    `;
+	element.insertAdjacentHTML("beforeend", template);
+}
+
+function renderCheckoutForm(element) {
+	const template = `
+        <section class="totalsum-block">
+			<div class="container totalsum-block__container">
+				<h3>Оформление заказа</h3>
+			</div>
+		</section>
+    `;
+	element.insertAdjacentHTML("beforeend", template);
 }
